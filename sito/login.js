b6311670a0e5f8f94c74eb84bca464ed552c8b9c
Dario <dario251102@gmail.com>
2020-08-24 15:25:14 +0200

    primo commit

---
+++ sito/login.js
@@ -0,0 +1,6 @@
+const bottone_scelta=document.getElementById("bottone_2")
+let pwd = ""
+function Registra(){
+	pwd = document.getElementById("display_pwd").value
+}
+bottone_scelta.onclick=Registra
