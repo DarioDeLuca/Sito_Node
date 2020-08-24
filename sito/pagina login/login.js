c81ed109b86fda1a5debcfedc50315b89ca48262
Dario <dario251102@gmail.com>
2020-08-24 16:31:47 +0200

    suddivisione in cartelle

--- sito/pagina login/login.js
+++ sito/pagina login/login.js
@@ -4,7 +4,6 @@ const bottone_scelta=document.getElementById("bottone_conferma")
 function Verifica(){
 	let email = document.getElementById("display_email").value
 	let pwd = document.getElementById("display_pwd").value
-	
 }
 bottone_scelta.onclick=Verifica
 
