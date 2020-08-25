298ca662be4b8ba41ac6f773072c9a77b0058a34
Eisendrach <manuel.barbieri16@gmail.com>
2020-08-25 16:56:59 +0200

    boh

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var express = require('express');
 var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname + '/views/paginaHome'))
+app.use(express.static(__dirname + '/views/paginaLogin'))
+app.use(express.static(__dirname + '/views/paginaRegistrazione'))
 // use res.render to load up an ejs view file
 
 // index page 
 app.get('/login', function(req, res) {
-    res.render('paginaLogin/login');
+    res.render(__dirname + '/views/paginaLogin/login');
 });
 
 app.get('/registrazione', function(req, res) {
@@ -17,7 +19,7 @@ app.get('/registrazione', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-  res.render('paginaHome/home');
+  res.render(__dirname + '/views/paginaHome/home');
 });
 
 
