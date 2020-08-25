ad94189ad8541d94a28b0822a7cf00a2185555f5
Eisendrach <manuel.barbieri16@gmail.com>
2020-08-25 17:31:52 +0200

    risolto errore nella lettura dei file nei .render()

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
-// server.js
-// load the things we need
+
 var express = require('express');
 var app = express();
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/views/paginaHome'))
-app.use(express.static(__dirname + '/views/paginaLogin'))
-app.use(express.static(__dirname + '/views/paginaRegistrazione'))
-// use res.render to load up an ejs view file
-
-// index page 
 app.get('/login', function(req, res) {
-    res.render(__dirname + '/views/paginaLogin/login');
+    res.render(__dirname + '/views/paginaLogin/login.ejs');
 });
 
 app.get('/registrazione', function(req, res) {
-  res.render('paginaRegistrazione/registra');
+  res.render(__dirname + '/views/paginaRegistrazione/registra.ejs');
 });
 
 app.get('/', function(req, res) {
-  res.render(__dirname + '/views/paginaHome/home');
+  res.render(__dirname + '/views/paginaHome/home.ejs');
 });
 
 
