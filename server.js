2d1034a779763ebd8f2ba590e5355f10ca3854b9
Dario <dario251102@gmail.com>
2020-08-25 17:40:01 +0200

    speriamo

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
-// server.js
-// load the things we need
+
 var express = require('express');
 var app = express();
-
-
-
-
-// use res.render to load up an ejs view file
-
-// index page 
-
-
 app.get('/login', function(req, res) {
-
-    res.render('paginaLogin/login.ejs');
-    
-
+    res.render(__dirname + '/views/paginaLogin/login.ejs');
 });
 
 app.get('/registrazione', function(req, res) {
-  res.render('paginaRegistrazione/registra.ejs');
- 
+  res.render(__dirname + '/views/paginaRegistrazione/registra.ejs');
+});
+
+app.get('/', function(req, res) {
+  res.render(__dirname + '/views/paginaHome/home.ejs');
 });
 
 
-
 app.listen(8080);
-console.log('bene');
+console.log('SERVER AVVIATO!');
