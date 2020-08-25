cfd7cc552784fc910cb127a1ce0d85667f5c7cea
Dario <dario251102@gmail.com>
2020-08-25 17:17:33 +0200

    commit con errori

--- server.js
+++ server.js
@@ -9,13 +9,21 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page 
+
+
+
 app.get('/login', function(req, res) {
-    res.render('paginaLogin/login');
+ 
+    res.render('paginaLogin/login.ejs');
+    
+
 });
 
 app.get('/registrazione', function(req, res) {
-  res.render('paginaRegistrazione/registra');
+  res.render('paginaRegistrazione/registra.ejs');
+ 
 });
+
 
 
 app.listen(8080);
--- views/paginaLogin/login.js
+++ views/paginaLogin/login.js
@@ -1,10 +1,12 @@
 const bottone_scelta=document.getElementById("bottone_conferma")
 let email=""
 let pwd=""
+
+
 function Verifica(){
 	email = document.getElementById("display_email").value
 	pwd = document.getElementById("display_pwd").value
-	
+	console.log("bela")
 }
 bottone_scelta.onclick=Verifica()
 
