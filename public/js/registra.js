088c46b2ec11d1298272d0f4023e5c22c2c34ba0
Dario <dario251102@gmail.com>
2020-08-29 11:49:52 +0200

    piccole modifiche

--- public/js/registra.js
+++ public/js/registra.js
@@ -1,7 +1,19 @@
 const bottone_scelta=document.getElementById("bottone_conferma")
+const bottone_home=document.getElementById("bottone_home")
+
+
 function RegistraValori(){
 	let email = document.getElementById("display_email").value
 	let pwd = document.getElementById("display_pwd").value
+	if (pwd==="ciao" && email==="caio@gmail.com"){
+		console.log("bela")
+	}
 }
-bottone_scelta.onclick=Registra
+
+function saluta(){
+	console.log("diocane")
+}
+
+bottone_scelta.onclick=RegistraValori
+
 
