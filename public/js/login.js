caa38f1be1501961e6d2c646997daef2660a85c0
Tommaso Siligardi <tomesse@outlook.it>
2020-09-05 12:20:19 +0200

    aggiunto verifica campi vuoti e condizioni pwd

--- public/js/login.js
+++ public/js/login.js
@@ -3,9 +3,14 @@
 function login(){
 	let email= document.getElementById("display_email").value
 	let pwd= document.getElementById("display_pwd").value
-	fetch(`/val?user=${email}&password=${pwd}`,{method: "GET"})
-	.then(res=>res.json())
-	.then(res=>{if (res.status==800) alert('error')})
+	if(email === "" || pwd===""){
+		alert("inserire i dati richiesti")
+	}
+	else {
+		fetch(`/val?user=${email}&password=${pwd}`,{method: "GET"})
+		.then(res=>res.json())
+		.then(res=>{if (res.status==800) alert('username o password errati')})
+	}
 }
 
 
--- public/js/registra.js
+++ public/js/registra.js
@@ -2,10 +2,16 @@
 function RegistraValori(){
 	let email = document.getElementById("display_email").value
 	let pwd = document.getElementById("display_pwd").value
-	fetch(`/reg?user=${email}&password=${pwd}`,{method: "POST"})
-	.then(res=>res.json())
-	.then(res=>{if (res.status==800) alert('error')
-else{alert('registrazione effettuata con successo')}})
+	if (email === "" || pwd==="" ){
+		alert("inserire i dati richiesti")
+	}
+	else {
+		if(pwd.length>8 && pwd.match(/[A-Z]/ && /[a-z]/ && /[0-9]/))
+		{fetch(`/reg?user=${email}&password=${pwd}`,{method: "POST"})
+		.then(res=>res.json())
+		.then(res=>{if (res.status==800) {alert('error')} else{alert('registrazione effettuata con successo')}})}
+		else {alert("la password non rispetta le condizioni ")}
+		}
 }
 
 
